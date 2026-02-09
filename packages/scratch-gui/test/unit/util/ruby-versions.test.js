75f55063af03a5e7fbaac14e19ac2dfb3ac03694
Kouji Takao <kouji.takao@gmail.com>
2026-02-09 15:06:49 +0900

    test: update ruby-versions persistence tests to use localStorage

--- packages/scratch-gui/test/unit/util/ruby-versions.test.js
+++ packages/scratch-gui/test/unit/util/ruby-versions.test.js
@@ -2,33 +2,24 @@ import {VERSION_1, VERSION_2} from '../../../src/lib/settings/ruby-version';
 import {detectRubyVersion, persistRubyVersion} from '../../../src/lib/settings/ruby-version/persistence';
 
 describe('ruby versions', () => {
-    const originalCookie = window.document.cookie;
     const originalDate = global.Date;
 
     beforeEach(() => {
-        // Clear cookies
-        const cookies = window.document.cookie.split(';');
-        for (let i = 0; i < cookies.length; i++) {
-            const cookie = cookies[i];
-            const eqPos = cookie.indexOf('=');
-            const name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
-            window.document.cookie = `${name}=;expires=Thu, 01 Jan 1970 00:00:00 GMT;path=/`;
-        }
+        window.localStorage.clear();
     });
 
     afterAll(() => {
-        window.document.cookie = originalCookie;
         global.Date = originalDate;
     });
 
     describe('persistence', () => {
-        test('returns the version stored in a cookie', () => {
-            window.document.cookie = 'smalruby:rubyVersion=2';
+        test('returns the version stored in localStorage', () => {
+            window.localStorage.setItem('smalruby:rubyVersion', '2');
             const version = detectRubyVersion();
             expect(version).toEqual(VERSION_2);
         });
 
-        test('returns v1 before 2026-04-01 when no cookie', () => {
+        test('returns v1 before 2026-04-01 when no preference', () => {
             const mockDate = new Date('2026-03-31T23:59:59Z');
             global.Date = class extends Date {
                 constructor (arg) {
@@ -44,7 +35,7 @@ describe('ruby versions', () => {
             expect(version).toEqual(VERSION_1);
         });
 
-        test('returns v2 after 2026-04-01 when no cookie', () => {
+        test('returns v2 after 2026-04-01 when no preference', () => {
             const mockDate = new Date('2026-04-01T00:00:01Z');
             global.Date = class extends Date {
                 constructor (arg) {
@@ -60,15 +51,14 @@ describe('ruby versions', () => {
             expect(version).toEqual(VERSION_2);
         });
 
-        test('persists version to cookie', () => {
+        test('persists version to localStorage', () => {
             persistRubyVersion(VERSION_2);
-            // jsdom's document.cookie might not behave exactly like a real browser
-            // but detectRubyVersion should be able to read what persistRubyVersion wrote.
+            expect(window.localStorage.getItem('smalruby:rubyVersion')).toEqual('2');
             expect(detectRubyVersion()).toEqual(VERSION_2);
         });
 
         test('throws error for invalid version', () => {
-            expect(() => persistRubyVersion(3)).toThrow('Invalid ruby version: 3');
+            expect(() => persistRubyVersion('3')).toThrow('Invalid ruby version: 3');
         });
     });
 });
